3d12cc4b692cc63967a5f8b335bdcd6b9d9ec3a1
mohit-kumar2511 <mohitkumarsolanki25112005@gmail.com>
2025-08-10 19:03:12 +0530

    Update first_reoccuring_Char.java

--- week4/First reoccuring caracter/first_reoccuring_Char.java
+++ week4/First reoccuring caracter/first_reoccuring_Char.java
@@ -1,1 +1,26 @@
+import java.util.HashSet;
+public class FirstRecurringCharacter {
+    public static Character findFirstRecurringCharacter(String s) {
+        HashSet<Character> seen = new HashSet<>();
+        
+        for (char c : s.toCharArray()) {
+            if (seen.contains(c)) {
+                return c;                
+            }
+            seen.add(c);
+        }
+        
+        return null;    
+    }
 
+    public static void main(String[] args) {
+        String input = "leetcode";
+        Character result = findFirstRecurringCharacter(input);
+        
+        if (result != null) {
+            System.out.println("First recurring character: " + result);
+        } else {
+            System.out.println("No recurring characters found.");
+        }
+    }
+}
